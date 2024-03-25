3c679ed58a79b74a5e8e5bc32c2735581ee7ee0c
berzyvonne <berzyvonne@gmail.com>
2024-03-24 22:29:05 -0400

    small fixes

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+// A user enters the website and finds a list of the names, dates, times, 
+// locations, and descriptions of all the parties that are happening.
+// Next to each party in the list is a delete button. The user clicks the 
+// delete button for one of the parties. That party is then removed from the list.
+// There is also a form that allows the user to enter information about a 
+// new party that they want to schedule. After filling out the form and submitting it, 
+// the user observes their party added to the list of parties.
+
 const COHORT = "2401-FTB-MT-WEB-PT";
 const APIURL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/events`;
 
@@ -40,7 +48,7 @@ const eventCards = state.events.map((event) => {
     return li;
   });
 
-  artistList.replaceChildren(...artistCards);
+  eventList.replaceChildren(...eventCards);
 
   //   update state with events from API
 
